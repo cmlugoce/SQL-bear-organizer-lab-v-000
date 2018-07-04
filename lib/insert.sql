c799dc9a56249c7c1eaf24f97f45bf196da3b952
Cristina Lugo <cmlugoce@syr.edu>
2018-07-04 15:44:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 7, "M", "Black", "tough" 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 1, "F", "brown", "sweet", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 5, "M", "brown", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 4, "F", "brown", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 4, "F", "black", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 3, "F", "black", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 4, "M", "black",)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 4, "F", "brown", "tough" 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 4, "F", "black", "tough" 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 3, "F", "black", "sweet", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 4, "M", "black", "killer", 0)
