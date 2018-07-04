24970399d2dd422cfe9b877d2eebce42304116fe
Cristina Lugo <cmlugoce@syr.edu>
2018-07-04 15:32:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 3, "M", "brown", "quiet", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(2, "Rowdy", 7, "M", )
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 7, "M", "Black", "tough" 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
